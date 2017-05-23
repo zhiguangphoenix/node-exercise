40c9b411135b2df7cbd47983309b8bc2eaeabae7
RickCZG <czg18366118377@yahoo.com>
2017-05-23 13:19:31 +0800

    170523

---
+++ 2.node key modules/node-globalObject-processArgv.js
@@ -0,0 +1,7 @@
+// console.log(process.argv);
+
+// console.log(process.stdout);
+
+// console.log(process.stdin);
+
+console.log(process.nextTick);
---
+++ 2.node key modules/node-keyModule-util.js
@@ -0,0 +1,43 @@
+var util = require("util");
+
+function Base(){
+    this.name = "base";
+    this.base = 1994;
+
+    this.sayHello = function(){
+        console.log("Hello" + this.name);
+    }
+}
+
+Base.prototype.showName = function(){
+    console.log(this.name);
+}
+
+function Sub(){
+    this.name = "sub";
+}
+
+util.inherits(Sub, Base);
+
+var objBase = new Base();
+objBase.showName();
+objBase.sayHello();
+console.log(objBase);
+
+var objSub = new Sub();
+objSub.showName();
+//objSub.sayHello();
+console.log(objSub);
+
+
+function Person(){
+    this.name = "czg";
+
+    this.toString = function(){
+        return this.name;
+    }
+}
+
+var obj = new Person();
+console.log(util.inspect(obj));
+console.log(util.inspect(obj, true));
