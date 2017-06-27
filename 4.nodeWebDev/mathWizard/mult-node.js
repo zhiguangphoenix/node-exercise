b2d60d766a06d3357bb82bc4882c5301666fa5d4
RickCZG <czg18366118377@yahoo.com>
2017-06-27 15:53:21 +0800

    mult-node

--- 4.nodeWebDev/mathWizard/mult-node.js
+++ 4.nodeWebDev/mathWizard/mult-node.js
@@ -8,7 +8,8 @@ exports.get = function (req, res) {
 	res.end(
 		htutil.page("Multiplication", htutil.navbar(), [
 				(!isNaN(req.a) && !isNaN(req.b)?
-					("<p class='result'>{a}*{b}={result}</p>".replace("{a}", req.a).replace("{b}", req.b).replace("{result}", req.a*req.b)):""),
+					("<p class='result'>{a}*{b}={result}</p>".replace("{a}", req.a).replace("{b}", req.b).replace("{result}", req.a*req.b))
+					:""),
 				"<p>输入两个数字做乘法</p>",
 				"<form name='mult' action='/mult' method='get'>",
 				"A: <input type='text' name='a'/><br/>",
