ef55635eb5f25945a371cddbabfd374be2144121
RickCZG <czg18366118377@yahoo.com>
2017-06-30 21:03:57 +0800

    httpsniffer

---
+++ 4.nodeWebDev/webServer/httpsniffer.js
@@ -0,0 +1,46 @@
+var util = require('util');
+var url = require('url');
+
+exports.sniffOn = function (server) {
+	server.on('request', function (req, res) {
+		util.log('event_request');
+		util.log(reqToString(req));
+	});
+
+	server.on('close', function (errno) {
+		util.log('event_close errno= ' + errno);
+	})
+
+	server.on('checkContinue', function (req, res) {
+		util.log('event_checkContinue');
+		util.log(reqToString(req));
+		res.writeContinue();
+	})
+
+	server.on('upgrade', function (req, socket, head) {
+		util.log('event_upgrade');
+		util.log(reqToString(req));
+	});
+
+	server.on('clientError', function () {
+		util.log('event_clientError');
+	})
+}
+
+
+var reqToString = function (req) {
+	var ret = 'request ' + req.method + ' ' + req.httpVersion + ' ' + req.url + '\n';
+	ret += JSON.stringify(url.parse(req.url, true)) + '\n';
+	var keys = Object.keys(req.headers);
+	for(var i = 0, l = keys.length;i < 1;i++) {
+		var key = keys[i];
+		ret += i + ' ' + key + ': ' + req.headers[key] + '\n';
+	}
+	if(req.trailers){
+		ret += req.trailers + '\n';
+		return ret;
+	}
+}
+
+exports.reqToString = reqToString;
+
