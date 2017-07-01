1fc0fc37017143b3c63212971f64aa6759335a72
RickCZG <czg18366118377@yahoo.com>
2017-07-02 00:00:51 +0800

    basicServer

--- 4.nodeWebDev/webServer/basicWebServer/basicserver.js
+++ 4.nodeWebDev/webServer/basicWebServer/basicserver.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var url = require('url');
+var http = require("http");
+var url = require("url");
 
 exports.createServer = function () {
 	var htserver = http.createServer(function (req, res) {
@@ -9,41 +9,35 @@ exports.createServer = function () {
 		processHeaders(req, res);
 		dispatchToContainer(htserver, req, res);
 	});
-
-	htserver.basicServer = { containers: [] };
-
-	/**
-	 * [addContainer description]
-	 * @param {[type]} host    匹配Host头的正则表达式
-	 * @param {[type]} path    匹配请求URL的政策表达式
-	 * @param {[type]} module  一个处理函数
-	 * @param {[type]} options 一个选项对象
-	 */
+	htserver.basicServer = {
+		containers: []
+	}
 	htserver.addContainer = function (host, path, module, options) {
-
-		if(lookupContainer(htserver, host, path) !== undefined){
-			throw new Error("Already mapped " + host + "/" + path);
+		if(lookupContainer(htserver, host, path) !== undefined) {
+			throw new Error("Already mapped " + host + "/path");
 		}
 		htserver.basicServer.containers.push({
-			host: host, path: path,
-			module: module, options: options
+			host: host,
+			path: path,
+			module: module,
+			options: options
 		});
 		return this;
 	}
-	
+
 	htserver.useFavIcon = function (host, path) {
-		return this.addContainer(host, "/favicon.ico", require('./faviconHandler'), { iconPath: path });
+		return this.addContainer(host, "/favicon.ico", require("./faviconHandler"), {iconPath: path});
 	}
 
-	htserver.docroot = function (host, path, rootPath) {
-		return this.addContainer(host, path, require('./staticHandler'), { docroot: rootPath });
+	htserver.docroot = function(host, path, rootPath) {
+		return this.addContainer(host, path, require("./staticHandler"), { docroot: rootPath });
 	}
 
 	return htserver;
 }
 
 /**
- * 用于检查container数组中是否有容器匹配HTTP请求中的host和path字段
+ * [ lookupContainer 检查container数组中是否有容器匹配HTTP请求中的host和path请求 ]
  * @param  {[type]} htserver [description]
  * @param  {[type]} host     [description]
  * @param  {[type]} path     [description]
@@ -53,10 +47,11 @@ var lookupContainer = function (htserver, host, path) {
 	for(var i = 0;i < htserver.basicServer.containers.length;i++) {
 		var container = htserver.basicServer.containers[i];
 		var hostMatches = host.toLowerCase().math(container.host);
-		if (hostMatches !== null && pathMatches !== null) {
+		var pathMatches = path.match(container.path);
+		if(hostMatches !== null && pathMatches !== null) {
 			return {
 				container: container,
-				host: host,
+				host: hostMatches,
 				path: pathMatches
 			};
 		}
@@ -64,24 +59,43 @@ var lookupContainer = function (htserver, host, path) {
 	return undefined;
 }
 
-
+/**
+ * [processHeaders 搜索req.headers数组以查找cookies和Host头部]
+ * @param  {[type]} req [description]
+ * @param  {[type]} res [description]
+ * @return {[type]}     [description]
+ */
 var processHeaders = function (req, res) {
 	req.basicServer.cookies = [];
 	var keys = Object.keys(req.headers);
-	for(var i = 0, l = keys.length;i < l;i++) {
+	for( var i = 0, l = keys.length;i < 1;i++) {
 		var hname = keys[i];
 		var hval = req.headers[hname];
-		
-		if (hname.toLowerCase() === 'host') {
+		if(hname.toLowerCase() === "host") {
 			req.basicServer.host = hval;
 		}
-
-		if(hname.toLowerCase() === 'cookie') {
+		if(hname.toLowerCase() === "cookies") {
 			req.basicServer.cookies.push(hval);
 		}
-
 	}
 }
+
+var dispatchToContainer = function (htserver, req, res) {
+	var container = lookupContainer(htserver, req.basicServer.host, req.basicServer.urlparsed.pathname);
+
+	if (container !== undefined) {
+		req.basicServer.hostMatches = container.host;
+		req.basicServer.pathMatches = container.path;
+		req.basicServer.container = container.container;
+		container.container.module.handle(req, res);
+	} else {
+		res.writeHead(404, { "Content-Type": "text/plain" });
+		res.end("no handler found for" + req.host + "/" + req.urlparsed.path);
+	}
+}
+
+
+
 
 
 
