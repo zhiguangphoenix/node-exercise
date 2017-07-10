6af3c817c02ba3a7ef79356669c45217bf9c672d
RickCZG <czg18366118377@yahoo.com>
2017-07-10 20:15:02 +0800

    general learing

--- 1.general learning/server.js
+++ 1.general learning/server.js
@@ -3,6 +3,8 @@ const moment = require('moment');
 
 var server = http.createServer(function (req, res) {
 
+	console.log(req instanceof http.IncomingMessage);
+	console.log(res instanceof http.IncomingMessage);
 	var thistime = moment().valueOf();
 	res.end(thistime.toString());
 })
