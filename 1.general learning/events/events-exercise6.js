00f1cb397e641e1d7c964d98a73fcf729407b82b
RickCZG <czg18366118377@yahoo.com>
2017-07-19 20:47:58 +0800

    node event

---
+++ 1.general learning/events/events-exercise6.js
@@ -0,0 +1,11 @@
+const MyEmitter = require('events');
+const myEmitter = new MyEmitter();
+
+let m = 0;
+myEmitter.once('event', () => {
+  console.log(++m);
+});
+myEmitter.emit('event');
+// Prints: 1
+myEmitter.emit('event');
+// Ignored
