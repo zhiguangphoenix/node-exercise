ae60220985e4312db91df692ceb83b8b6e5a2f23
RickCZG <czg18366118377@yahoo.com>
2017-07-20 11:10:06 +0800

    events

---
+++ 1.general learning/events/events-exercise9.js
@@ -0,0 +1,28 @@
+const MyEmitter = require('events');
+const myEmitter = new MyEmitter();
+
+const callbackA = () => {
+  console.log('A');
+  myEmitter.removeListener('event', callbackB);
+};
+
+const callbackB = () => {
+  console.log('B');
+};
+
+myEmitter.on('event', callbackA);
+
+myEmitter.on('event', callbackB);
+
+// callbackA removes listener callbackB but it will still be called.
+// Internal listener array at time of emit [callbackA, callbackB]
+myEmitter.emit('event');
+// Prints:
+//   A
+//   B
+
+// callbackB is now removed.
+// Internal listener array [callbackA]
+myEmitter.emit('event');
+// Prints:
+//   A
