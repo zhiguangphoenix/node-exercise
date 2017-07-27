cd85bcef5dc6e15e527d3efe972ee06bdc153948
RickCZG <czg18366118377@yahoo.com>
2017-07-27 14:21:19 +0800

    index

--- 5.koa/my-exercise/blog/index.js
+++ 5.koa/my-exercise/blog/index.js
@@ -19,8 +19,8 @@ app.use(koaBody());
 
 router.get('/', list)
 	.get('/post/new', add)
-	// .get('/post/:id', show)
-	// .get('/post', create);
+	.get('/post/:id', show)
+	.post('/post', create);
 
 app.use(router.routes());
 
@@ -33,7 +33,24 @@ async function add(ctx) {
 	await ctx.render('new');
 }
 
+async function show(ctx) {
+	const id = ctx.params.id;
+	const post = posts[id];
 
+	if(!post)
+		ctx.throw(404, 'invalid post id');
+	await ctx.render('show', {post: post});
+}
+
+
+async function create(ctx) {
+	const post = ctx.request.body;
+	const id = posts.push(post) - 1;
+	post.created_at = new Date();
+	post.id = id;
+
+	ctx.redirect('/');
+}
 
 
 
